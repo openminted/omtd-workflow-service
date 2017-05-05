38c50f21d72e4b53d55b2ca4d55c53d907e21117
greenwoodma <mark@dcs.shef.ac.uk>
2017-05-05 15:08:20 +0100

    code formatting

--- src/main/java/eu/openminted/workflow/service/WorkflowServiceImpl.java
+++ src/main/java/eu/openminted/workflow/service/WorkflowServiceImpl.java
@@ -36,53 +36,61 @@ import eu.openminted.workflow.api.WorkflowService;
 @EnableAutoConfiguration
 public class WorkflowServiceImpl implements WorkflowService {
 
-    @RequestMapping("/")
-    String home() {
-        return "omtd-workflow-service";
-    }
+	@RequestMapping("/")
+	String home() {
+		return "omtd-workflow-service";
+	}
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(WorkflowService.class, args);
-    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(WorkflowService.class, args);
+	}
 
-    @Override
-    public String execute(WorkflowJob workflowJob) throws WorkflowException {
-	//get workflow from job, it's a Component instance so will then need to get it out of there
-	//question is what does that object contain and where is the workflow file actually stored?
+	@Override
+	public String execute(WorkflowJob workflowJob) throws WorkflowException {
+		// get workflow from job, it's a Component instance so will then need to
+		// get it out of there
+		// question is what does that object contain and where is the workflow
+		// file actually stored?
 
-	//download the corpus from the OMTD-STORE using the REST client. This should get us a folder
-	//but not sure the format of the contents has been fixed yet
+		// download the corpus from the OMTD-STORE using the REST client. This
+		// should get us a folder
+		// but not sure the format of the contents has been fixed yet
 
-	//run the workflow over the corups. can this be done in one shot or do we need to send each
-	//document in turn. Might depend on how the workflow is written? 
+		// run the workflow over the corups. can this be done in one shot or do
+		// we need to send each
+		// document in turn. Might depend on how the workflow is written?
 
-	//return an ID that can be used to lookup the workflow later so you can get the status.
-	//this probably means spawning a thread for the actual execution so we can monitor the
-	//status of the Galaxy job as it runs
-        return null;
-    }
+		// return an ID that can be used to lookup the workflow later so you can
+		// get the status.
+		// this probably means spawning a thread for the actual execution so we
+		// can monitor the
+		// status of the Galaxy job as it runs
+		return null;
+	}
 
-    @Override
-    public void cancel(String workflowExecutionId)  throws WorkflowException {
+	@Override
+	public void cancel(String workflowExecutionId) throws WorkflowException {
 
-    }
+	}
 
-    @Override
-    public void pause(String workflowExecutionId) throws WorkflowException {
+	@Override
+	public void pause(String workflowExecutionId) throws WorkflowException {
 
-    }
+	}
 
-    @Override
-    public void resume(String workflowExecutionId) throws WorkflowException {
+	@Override
+	public void resume(String workflowExecutionId) throws WorkflowException {
 
-    }
+	}
 
-    @Override
-    public ExecutionStatus getExecutionStatus(String workflowExecutionId) throws WorkflowException {
-	//assuming execution has started this should be a case of polling galaxy as in the existing
-	//demo code, to figure out where it's at, we just then need to wrap that as an ExecutionStatus
-        return null;
-    }
+	@Override
+	public ExecutionStatus getExecutionStatus(String workflowExecutionId) throws WorkflowException {
+		// assuming execution has started this should be a case of polling
+		// galaxy as in the existing
+		// demo code, to figure out where it's at, we just then need to wrap
+		// that as an ExecutionStatus
+		return null;
+	}
 
 	/**
 	 * Creates a new history to hold input and output files
