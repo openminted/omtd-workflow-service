32b95a8072a34f627dbc89693a8f41cc25c970ff
greenwoodma <mark@dcs.shef.ac.uk>
2017-05-18 14:17:42 +0100

    removed an unused import

--- src/main/java/eu/openminted/workflow/service/WorkflowServiceImpl.java
+++ src/main/java/eu/openminted/workflow/service/WorkflowServiceImpl.java
@@ -12,7 +12,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
-import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Date;
 import java.util.HashMap;
