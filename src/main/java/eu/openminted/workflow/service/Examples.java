b87f3344218637c731cf195864196d2489355193
galanisd <galanisd@gmail.com>
2017-06-13 15:04:04 +0300

    Clean up.

--- src/main/java/eu/openminted/workflow/service/Examples.java
+++ src/main/java/eu/openminted/workflow/service/Examples.java
@@ -8,20 +8,13 @@ public class Examples {
 
 	public static void main(String args[]){
 		String galaxyInstanceUrl = "http://snf-754063.vm.okeanos.grnet.gr";
-		String galaxyApiKey = "";
+		String galaxyApiKey = "<...>";
 		
 		Galaxy galaxy = new Galaxy(galaxyInstanceUrl, galaxyApiKey);
+		
 		//galaxy.runWorkflow("/home/ilsp/Desktop/DG/OMTD/omtd-simple-workflows/testInput/", "DGTest1");
-		//galaxy.runWorkflow("C:/Users/galanisd/Desktop/smallPDFs/", "DGTest1", "C:/Users/galanisd/Desktop/smallPDFsOut/");
-		
-		//galaxy.runWorkflow("C:/Users/galanisd/Desktop/Dimitris/EclipseWorkspaces/ILSPMars/omtd-simple-workflows/testInput/", "DGTest1", "C:/Users/galanisd/Desktop/smallPDFsOut/");
-		
-		try {
-			Path tmpFile = Files.createTempFile(null, "file.pdf");
-			System.out.println(tmpFile.toFile().getAbsolutePath());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//galaxy.runWorkflow("C:/Users/galanisd/Desktop/smallPDFs/", "DGTest1", "C:/Users/galanisd/Desktop/smallPDFsOut/");		
+		galaxy.runWorkflow("C:/Users/galanisd/Desktop/Dimitris/EclipseWorkspaces/ILSPMars/omtd-simple-workflows/testInput/", "DGTest1", "C:/Users/galanisd/Desktop/smallPDFsOut/");
+				
 	}
 }
